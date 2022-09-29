a0dbbf02934c51b6621947bd2d88291922531b8b
shilpykrisna <113418765+shilpykrisna@users.noreply.github.com>
2022-09-29 18:48:47 +0530

    Update Mux code.vhdl

--- Mux code.vhdl
+++ Mux code.vhdl
@@ -4,7 +4,7 @@ entity mux 2*1 is
   port(i0,i1,s: in std_logic;
        y: out std_logic);
   end;
-  architecture mydesign of MUX 2*1 is 
+  architecture mydesign of mux 2*1 is 
     begin 
       y<= (i1 and s) or ((not s) and i0);
     end mydesign;
